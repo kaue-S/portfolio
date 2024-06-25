76f3bf7e0014440be3ecf0965206406632f253fb
kaue-S <kauesilva38@hotmail.com>
2024-06-25 16:13:43 -0300

    menu hamburguer

--- src/components/Navegacao.jsx
+++ src/components/Navegacao.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { GraduationCap, Laptop, Mail, Menu, MenuIcon, UserRound, X, github } from 'lucide-react'
 import Link from 'next/link'
+import Hamburger from 'hamburger-react'
 
 export default function Navegacao() {
   const [menuAberto, setMenuAberto] = useState(false)
@@ -20,12 +21,13 @@ export default function Navegacao() {
         </ul>
       </nav>
 
-      <button onClick={toggleMenu} className="border rounded-full bg-amber-400 p-2 hover:bg-amber-300 lg:hidden">
-        {menuAberto ?  <X strokeWidth={1}/> : <MenuIcon strokeWidth={1}/>}
+      <button onClick={toggleMenu} className="border rounded-full bg-amber-400 lg:hidden">
+        {/* {menuAberto ?  <X strokeWidth={1}/> : <MenuIcon strokeWidth={1}/>} */}
+        <Hamburger size={20} label="icone de abrir e fechar o menu"/>
       </button>
 
       {menuAberto && (
-        <nav className="bg-white absolute top-0 mt-12 mr-11 right-0 w-60 h-60 shadow-md border">
+        <nav className="bg-white absolute top-0 mt-14 mr-12 right-0 w-60 h-60 shadow-md border">
           <ul className="flex flex-col justify-around text-start h-full text-xl font-semibold text-black">
               <li className="py-2 hover:bg-amber-400"><Link className="pl-8 flex gap-4" href={""}><UserRound /> Sobre</Link></li>
               <li className="py-2 hover:bg-amber-400"><Link className="pl-8 flex gap-4" href={""}><Laptop /> Projetos</Link></li>
