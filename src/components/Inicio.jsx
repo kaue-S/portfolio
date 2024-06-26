76764468034a55864b15339b146b13de55c20db6
kaue-S <kauesilva38@hotmail.com>
2024-06-26 00:31:50 -0300

    Criação dos UI botões

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,13 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
+import Botao from './ui/Botao'
 
 export default function Inicio() {
   return (
     <section className="">
-        <article className="flex items-center justify-center lg:gap-3">
-            <Image className="lg:w-16 lg:h-16" src={"/images/mao.png"} alt='icone de uma mão acenando' width={40} height={60}/>
-            <h1 className="xl:text-5xl lg:text-4xl sm:text-3xl text-xl whitespace-nowrap font-bold">Olá, sou o <span className="text-amber-400 ">Kauê Evangelista</span> </h1>
+        <article className=" ">
+            <div className="flex items-center justify-center lg:gap-3">
+                <Image className="lg:w-16 lg:h-16" src={"/images/mao.png"} alt='icone de uma mão acenando' width={40} height={60}/>
+                <h1 className="xl:text-5xl lg:text-4xl sm:text-3xl text-xl font-bold">Olá, sou o <span className="text-amber-400 ">Kauê Evangelista</span></h1>
+            </div>
+            <h2 className="text-center xl:text-4xl lg:text-3xl sm:text-2xl text-lg font-semibold">Desenvolvedor Web</h2>
         </article>
+        <Botao />
     </section>
   )
 }
---
+++ src/components/ui/Botao.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Botao() {
+  return (
+    <button className="px-2 border rounded-full">
+        teste
+    </button>
+  )
+}
