7c905ba54e736655c4ba08fd1b09b2d97b848fcb
kaue-S <kauesilva38@hotmail.com>
2024-06-27 11:47:19 -0300

    Update Inicio.jsx

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -9,21 +9,21 @@ export default function Inicio() {
 
         <article className="">
             <div className="flex items-center justify-center lg:gap-3">
-                <Image className="lg:w-16 lg:h-16" src={"/images/mao.png"} alt='icone de uma mão acenando' width={40} height={60}/>
-                <h1 className="xl:text-5xl lg:text-4xl sm:text-3xl text-2xl font-bold ">Olá, sou o <span className="text-amber-400 ">Kauê Evangelista</span></h1>
+                <Image className="lg:w-16 lg:h-16 max-[402px]:ml-2" src={"/images/mao.png"} alt='icone de uma mão acenando' width={40} height={60}/>
+                <h1 className="xl:text-5xl lg:text-4xl sm:text-3xl text-2xl font-bold text-center max-[402px]:pr-8">Olá, sou o <span className="text-amber-400 ">Kauê Evangelista</span></h1>
             </div>
             <h2 className="text-center xl:text-4xl lg:text-3xl sm:text-2xl text-lg font-semibold">Desenvolvedor Web</h2>
         </article>
 
         <article className=" mt-10 flex justify-center gap-5">
           <a href="/images/curriculo.pdf" download>
-            <button className='flex gap-2 items-center font-semibold text-amber-400 p-3 rounded-full border-2 border-amber-400 hover:text-slate-700 bg-amber-400 bg-opacity-30 hover:bg-amber-400 hover:transition-colors'>
+            <button className='flex gap-2 items-center font-bold text-amber-400 p-3 rounded-full border-2 border-amber-400 hover:text-black bg-amber-400 bg-opacity-30 hover:bg-amber-400 hover:transition-colors'>
               <Download /> Currículo
             </button>
           </a>
 
           <a href="">
-            <button className='flex gap-2 items-center font-semibold text-slate-700 p-3 px-6 rounded-full border-2 border-amber-400 bg-amber-400'>
+            <button className='flex gap-2 items-center font-bold text-black p-3 px-5 rounded-full border-2 border-amber-400 bg-amber-400'>
               Sobre Mim
             </button>
           </a>
