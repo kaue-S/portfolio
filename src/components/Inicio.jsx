1cd568ee5718cc6f6c3c1f21bfdb819ca51daa00
kaue-S <kauesilva38@hotmail.com>
2024-06-28 00:50:04 -0300

    comit

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -8,12 +8,15 @@ export default function Inicio() {
   return (
     <section className="">
 
-      <article className="">
-            <div className="flex items-center justify-center lg:gap-3">
-                <Image className="lg:w-16 lg:h-16 max-[402px]:ml-2" src={"/images/mao.png"} alt='icone de uma mão acenando' width={40} height={60}/>
-                <h1 className="xl:text-5xl lg:text-4xl sm:text-3xl text-2xl font-bold text-center max-[402px]:pr-8">Olá, sou o <span className="text-amber-400 ">Kauê Evangelista</span></h1>
+      <article  className="flex flex-col md:flex-row justify-center items-center gap-10 md:h-80 border ">
+            <div className="flex flex-col items-center justify-center lg:gap-3">
+                <Image className="lg:w-16 lg:h-16" src={"/images/mao.png"} alt='icone de uma mão acenando' width={40} height={60}/>
+                <h1 className="xl:text-5xl lg:text-4xl sm:text-3xl text-2xl font-bold text-center ">Olá, sou o <span className="text-amber-400 ">Kauê Evangelista</span></h1>
+              <h2 className="text-center xl:text-4xl lg:text-3xl sm:text-2xl text-lg font-semibold">Desenvolvedor Web</h2>
             </div>
-            <h2 className="text-center xl:text-4xl lg:text-3xl sm:text-2xl text-lg font-semibold">Desenvolvedor Web</h2>
+            <figure className="w-52 h-52 rounded-full overflow-hidden lg:mt-7">
+              <Image className='' src={"/images/foto.jpeg"} alt='minha foto' width={1186} height={1599} priority/>
+            </figure>
       </article>
 
       <article className=" mt-10 flex justify-center gap-5">
