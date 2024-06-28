6227073971703abb9bb1a95956b9a2650a6b1eb4
kaue-S <kauesilva38@hotmail.com>
2024-06-27 23:48:33 -0300

    comit

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
-import Botao from './ui/Botao'
 import { Download } from 'lucide-react'
 import { stackDados } from '@/api/stacks'
 
 export default function Inicio() {
-  console.log(stackDados);
+  const primeirosQuatros = stackDados.slice(0, 4);
+  const ultimosQuatros = stackDados.slice(4, 8);
+
   return (
     <section className="">
 
@@ -33,18 +34,42 @@ export default function Inicio() {
 
       <article className="flex flex-wrap justify-center gap-6">
 
-        {stackDados.map((item) =>(
-          <div className="bg-white text-black flex items-center gap-1 font-semibold p-3 rounded-full" key={item.id}>
-            <Image src={item.icone} alt={`icone do ${item.nome}`} width={25} height={25} />
+        {stackDados.map((item, index) =>(
+          <div className="bg-white text-black flex items-center gap-1 font-semibold p-3 rounded-full" key={index}>
+            <Image src={item.icone} alt={`icone do ${item.nome}`} width={25} height={25}/>
             <p>
               {item.nome}
             </p>
           </div>
+        ))}
+      </article>
 
+      {/* <div className="flex justify-center items-center gap-6">
+
+      <div className='flex justify-between flex-col gap-7'>
+        {primeirosQuatros.map((item, index) =>(
+          <div key={index} className='flex bg-white text-black items-center gap-1 font-semibold p-3 rounded-full'>
+            <Image src={item.icone} alt={`icone do ${item.icone}`} width={25} height={25}/>
+            <p>{item.nome}</p>
+          </div>
         ))}
 
-      </article>
-        <Image src={"/images/foto.jpeg"} alt="foto do de perfil do" width={500} height={500}/>
+      </div>
+
+      <figure className="w-1/5 h-2/5 ">
+        <Image className="" src={"/images/foto.jpeg"} alt="foto do de perfil do" width={1156} height={1599} priority/>
+      </figure>
+
+      <div>
+        {ultimosQuatros.map((item, index) =>(
+          <div key={item} className='flex bg-white text-black items-center gap-1 font-semibold p-3 rounded-full'>
+            <Image src={item.icone} alt={`icone do ${item.icone}`} width={25} height={25}/>
+            <p>{item.nome}</p>
+          </div>
+        ))}
+      </div>
+      </div> */}
+
 
     </section>
   )
