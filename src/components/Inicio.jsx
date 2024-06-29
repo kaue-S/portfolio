bc226443145fb8531e6edbe036a91fc1eb866133
kaue-S <kauesilva38@hotmail.com>
2024-06-29 11:47:55 -0300

    finalizando parte inicial

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -8,29 +8,32 @@ export default function Inicio() {
   return (
     <section className="">
 
-      <article  className="flex flex-col md:flex-row justify-center items-center gap-10 md:h-80 border ">
+      <article  className="flex flex-col-reverse md:flex-row justify-center items-center gap-7 md:gap-20 md:h-80">
             <div className="flex flex-col items-center justify-center lg:gap-3">
+
                 <Image className="lg:w-16 lg:h-16" src={"/images/mao.png"} alt='icone de uma mão acenando' width={40} height={60}/>
+
                 <h1 className="xl:text-5xl lg:text-4xl sm:text-3xl text-2xl font-bold text-center ">Olá, sou o <span className="text-amber-400 ">Kauê Evangelista</span></h1>
-              <h2 className="text-center xl:text-4xl lg:text-3xl sm:text-2xl text-lg font-semibold">Desenvolvedor Web</h2>
+
+                <h2 className="text-center xl:text-4xl lg:text-3xl sm:text-2xl text-lg font-semibold">Desenvolvedor Web</h2>
+
+              <a className='mt-4' href="/images/curriculo.pdf" download>
+                <button className='flex gap-2 items-center font-semibold text-amber-400 p-3 rounded-full border-2 border-amber-400 hover:text-black bg-amber-400 bg-opacity-30 hover:bg-amber-400 hover:transition-colors'>
+                <Download /> Currículo
+                </button>
+              </a>
+
             </div>
-            <figure className="w-52 h-52 rounded-full overflow-hidden lg:mt-7">
-              <Image className='' src={"/images/foto.jpeg"} alt='minha foto' width={1186} height={1599} priority/>
-            </figure>
+
+            <div className="border-4 animate-fade-down border-amber-400 rounded-full p-1">
+              <figure className="w-40 h-40 md- xl:w-52 xl:h-52 rounded-full overflow-hidden">
+                <Image className='' src={"/images/foto.jpeg"} alt='minha foto' width={1186} height={1599} priority/>
+              </figure>
+            </div>
       </article>
 
       <article className=" mt-10 flex justify-center gap-5">
-          <a href="/images/curriculo.pdf" download>
-            <button className='flex gap-2 items-center font-semibold text-amber-400 p-3 rounded-full border-2 border-amber-400 hover:text-black bg-amber-400 bg-opacity-30 hover:bg-amber-400 hover:transition-colors'>
-              <Download /> Currículo
-            </button>
-          </a>
-
-          <a href="">
-            <button className='flex gap-2 items-center font-bold text-black p-3 px-5 rounded-full border-2 border-amber-400 bg-amber-400'>
-              Sobre Mim
-            </button>
-          </a>
+        <h1 className="text-2xl font-bold">Sobre Mim</h1>
       </article>
 
       <article className="flex flex-wrap justify-center gap-6">
--- src/components/Navegacao.jsx
+++ src/components/Navegacao.jsx
@@ -22,12 +22,11 @@ export default function Navegacao() {
       </nav>
 
       <button onClick={toggleMenu} className="border rounded-full bg-amber-400 lg:hidden">
-        {/* {menuAberto ?  <X strokeWidth={1}/> : <MenuIcon strokeWidth={1}/>} */}
         <Hamburger size={20} label="icone de abrir e fechar o menu"/>
       </button>
 
       {menuAberto && (
-        <nav className="bg-white absolute top-0 mt-14 mr-12 right-0 w-60 h-60 shadow-md border animate-flip-down">
+        <nav className="bg-white absolute top-0 mt-14 mr-12 right-0 w-60 h-60 shadow-md border animate-flip-down z-50">
           <ul className="flex flex-col justify-around text-start h-full text-xl font-semibold text-black">
               <li className="py-3 hover:bg-amber-400"><Link className="pl-8 flex gap-4" href={""}><UserRound /> Sobre</Link></li>
               <li className="py-3 hover:bg-amber-400"><Link className="pl-8 flex gap-4" href={""}><Laptop /> Projetos</Link></li>
