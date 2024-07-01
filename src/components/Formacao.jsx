7727ed54981254ce794b8124225392c8c0b63707
kaue-S <kauesilva38@hotmail.com>
2024-07-01 19:32:20 -0300

    Update Formacao.jsx

--- src/components/Formacao.jsx
+++ src/components/Formacao.jsx
@@ -10,22 +10,22 @@ export default function Formacao() {
         <h1 className=" absolute xl:text-5xl lg:text-4xl sm:text-3xl text-2xl font-bold text-amber-400 tracking-wide">Formação</h1>
       </div>
 
-      <article className=" bg-slate-50 mt-10 m-auto sm:w-4/5 lg:w-[600px] p-4 md:p-6 rounded-3xl flex flex-col justify-around gap-5">
+      <article className=" bg-slate-50 mt-10 m-auto sm:w-4/5 lg:w-[600px] p-4 md:p-6 rounded-3xl flex flex-col justify-around ">
         <div className="flex justify-between">
           <p className="text-amber-400 font-semibold bg-zinc-900 px-3 md:px-4 py-2 flex items-center rounded-full gap-2 shadow-lg"><CalendarDays width={20}/> 2023 - 2024</p>
           <p className="text-black bg-amber-200 font-semibold flex items-center px-2 md:px-4 rounded-full gap-2 shadow-lg "><Image src={"/images/education.png"} alt="figura de formatura" width={20} height={20}/>Técnico</p>
         </div>
-        <h1 className="text-black font-bold text-2xl">Técnico em informática para internet</h1>
+        <h1 className="text-black font-bold text-2xl mt-8 mb-8">Técnico em informática para internet</h1>
 
-        <div className="flex gap-3 items-center">
-          <div className="border-2 rounded-full">
-            <Image src={"/images/school.png"} width={35} height={35} alt='figura de escola' className="text-amber-400 tag p-2 rounded-full shadow"/>
+        <div className="flex gap-2 items-center mb-5">
+          <div className="">
+            <Image src={"/images/school.png"} width={30} height={30} alt='figura de escola' className="text-amber-400 tag "/>
           </div>
           <p className="text-black font-semibold">Senac</p>
         </div>
 
-        <div className="flex gap-3 items-center">
-          <div className="border-2 rounded-full"><Image src={"/images/gps.png"} alt='icone localização' width={35} height={35} className="text-amber-400 tag p-2 rounded-full shadow"/></div>
+        <div className="flex gap-2 items-center">
+          <div className=""><Image src={"/images/gps.png"} alt='icone localização' width={30} height={30} className="text-amber-400 tag"/></div>
           <p className="text-black font-semibold">São Paulo - SP</p>
         </div>
 
