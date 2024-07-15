776e9a15968965bf8258f7ecdf14ee788c7b13bf
kaue-S <kauesilva38@hotmail.com>
2024-07-15 00:24:49 -0300

    comit

---
+++ src/api/diplomas.js
@@ -0,0 +1,15 @@
+
+export const diplomas = [
+    {
+        "titulo": "Desenvolvimento Front-End",
+        "imagem": "/images/diploma-front-end.jpeg"
+    },
+    {
+        "titulo": "Desenvolvimento Back-End",
+        "imagem": "/images/diploma-back-end.jpeg"
+    },
+    {
+        "titulo": "Desenvolvimento Mobile",
+        "imagem": "/images/diploma-mobile.jpeg"
+    },
+]
--- src/components/Certificados.jsx
+++ src/components/Certificados.jsx
@@ -1,7 +1,7 @@
-import { CalendarDays, Eye } from 'lucide-react'
+import { Eye } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
-
+import { diplomas } from '@/api/diplomas'
 
 
 export default function Certificados() {
@@ -13,7 +13,27 @@ export default function Certificados() {
         </div>
 
         <div className="mt-10 sm:w-4/5 m-auto flex max-lg:flex-col lg:flex lg:flex-wrap lg:justify-around gap-10 xl:px-5">
-            <article className=" bg-slate-50 rounded-3xl p-6 lg:w-[45%]">
+            {diplomas.map((diploma) =>(
+                <article className=" bg-slate-50 rounded-3xl p-6 lg:w-[45%]" key={diploma.id}>
+                <div className="flex items-center flex-wrap justify-between gap-5">
+                    <figure className="bg-black flex items-center rounded-full p-1 pr-3 gap-2 justify-around shadow-lg">
+                        <Image src={"/images/senac1.png"} alt="logotipo da instituição senac" width={50} height={55} className="rounded-full bg-white"/>
+                            <figcaption>
+                                <p className="text-amber-400 font-bold text-nowrap text-xl pr-5">Senac </p>
+                            </figcaption>
+                    </figure>
+                        
+                    <Image src={"/images/diploma.png"} alt="figura de um diploma" width={50} height={50}/>
+                </div>
+
+                <h1 className="text-black font-bold text-2xl mt-8 mb-8">{diploma.titulo}</h1>
+                
+                <a href="" className="bg-gray-300 inline-flex border p-3 gap-2 border-black text-black font-semibold rounded-full w-40">
+                    <Eye strokeWidth={2}/> Ver diploma
+                </a>
+            </article>
+            ))}
+            {/* <article className=" bg-slate-50 rounded-3xl p-6 lg:w-[45%]">
                 <div className="flex items-center flex-wrap justify-between gap-5">
                     <figure className="bg-black flex items-center rounded-full p-1 pr-3 gap-2 justify-around shadow-lg">
                         <Image src={"/images/senac1.png"} alt="logotipo da instituição senac" width={50} height={55} className="rounded-full bg-white"/>
@@ -68,7 +88,7 @@ export default function Certificados() {
                 <a href="" className="bg-gray-300 inline-flex border p-3 gap-2 border-black text-black font-semibold rounded-full w-40">
                     <Eye strokeWidth={2}/> Ver diploma
                 </a>
-            </article>
+            </article> */}
         </div>
     </section>
   )
